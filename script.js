4a25ab66a539294dee74016c85cb32b627870df3
Henrique Moura <henrique_hgm1996@hotmail.com>
2023-10-26 21:56:20 +0000

    Realiza ajustes no header

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+function showIconHome() {
+    const homeBtn = document.querySelector('.btn-fixed');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY <= 300) {
+            homeBtn.classList.remove('active')
+        } else {
+            homeBtn.classList.add('active')
+        }
+    });
+}
+
+showIconHome();
