56878c992f6b73818616d7e0dc0989306b0a9bfe
Henrique Moura <henrique_hgm1996@hotmail.com>
2023-10-27 00:42:05 +0000

    Adiciona alerta a home

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function showIconHome() {
     });
 }
 
+function alertDev() {
+    setTimeout(() => {
+        if(!localStorage.getItem('alertMessage')) {
+            window.alert('Olá, que bom te ter aqui. Está mensagem é apenas para informar que o site está em seu modelo MVP e ganhando atualização constantes! Caso queria dar um feedback, sinta-se à vontade para entrar em contato comigo. Forte Abraço!')
+            localStorage.setItem('alertMessage', 'yes');
+        }
+    }, 3000)
+}
+
 showIconHome();
+alertDev();
